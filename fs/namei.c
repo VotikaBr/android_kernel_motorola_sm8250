30f7f9496ae5954fa2ee54fe4028ab9be401c7c6
VotikaBr <rafael1.spindola@gmail.com>
2026-05-31 23:15:22 -0300

    SUSFS: fix SUS_PATH errno leak and bump KSU min compat

--- fs/namei.c
+++ fs/namei.c
@@ -2290,18 +2290,17 @@ static int link_path_walk(const char *name, struct nameidata *nd)
 		u64 hash_len;
 		int type;
 
-		err = may_lookup(nd);
-		if (err)
-			return err;
-
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
 		dentry = nd->path.dentry;
 		if (dentry->d_inode && susfs_is_inode_sus_path(dentry->d_inode)) {
-			// - No need to dput() here
-			// - return -ENOENT here since it is walking the sub path of sus path
+			/* Walking inside a hidden path should look like non-existent. */
 			return -ENOENT;
 		}
 #endif
+
+		err = may_lookup(nd);
+		if (err)
+			return err;
 
 		hash_len = hash_name(nd->path.dentry, name);
 
